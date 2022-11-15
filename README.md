c04447269e6e84477f944356671653d42db90462
Henry Roncancio <henryroncanciovelandia@gmail.com>
2022-11-15 13:32:20 -0500

    Added info to readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# objectclassification
+# Object Classification for Robotic Applications
+
+
 Garbage Classification for selection
+
+
+
+
+
+In order for this code to code you will need to install a few packages 
+
+```shell script
+pip3 install -r requirements.txt
+```
+
+Recomended instructions
+
+
--- ai.py
+++ ai.py
@@ -1,1 +1,21 @@
-print("hello")
+import tensorflow as tf
+import tensorflow_hub as hub
+import matplotlib.pyplot as plt
+import os, cv2
+
+Px = 224
+
+
+modelo = tf.keras.models.load_model(filepath='./ai_models/modeloProV2_1.h5', custom_objects={'KerasLayer':hub.KerasLayer}, compile=True, options=None)
+#print(modelo)
+
+img = cv2.imread('./test_images/f1_g7.png')
+img = cv2.resize(img,(Px,Px))
+print(img.shape)
+
+print(modelo.predict(img.reshape((1,224,224,3))))
+plt.imshow(cv2.resize(img,(Px,Px)))
+
+cv2.imshow('ImageWindow', img)
+
+cv2.waitKey()
